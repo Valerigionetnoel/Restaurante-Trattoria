68ec4865d40ee6130d08e931be2e8ebe973f4daf
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-21 20:28:40 -0300

    did most of the login

---
+++ client/pages/Login.js
@@ -0,0 +1,41 @@
+import React, { userState } from 'react';
+import { Link } from 'react-router-dom';
+import { userMutation } from '@apollo/client';
+import { LOGIN_USER, ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token);
+        } catch (err) {
+            console.error(err);
+        }
+
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
+};
+
+export default Login;
