90a18ad6875e0172809205d7c8ff1879dd21a6d2
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-21 19:02:16 -0300

    Did the review model

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,2 +1,23 @@
 const { Schema, model} = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+
+const reviewSchema = new Schema ({
+    reviewText: {
+        type: String,
+        required: 'You need to leave a review!',
+        minlength: 1,
+        maxlength: 280,
+        trim: true,
+    },
+    reviewAuthor: {
+        type: Schema.Types.ObjectId,
+        ref: User
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Review = model('Review', reviewSchema);
+
+module.exports = Review;
