927e93b7680b4242007d909419571075c0f399bc
Victoria Benoit <vrose834@gmail.com>
2023-06-21 20:52:11 -0300

    Got the picture for the menu and the 5 buttons

---
+++ client/src/styled/Menu.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledMenuContainer = styled.div`
+
+
+
+`;
+
+export const StyledMenuButtonContainer = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-evenly;
+gap: 1em;
+
+button{
+    width: 25%;
+}
+button:hover{
+    width: 25%;
+}
+
+`;
+
