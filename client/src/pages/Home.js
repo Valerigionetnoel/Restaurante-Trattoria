ea06d230f562fad75025397e150cb357dafe2810
Victoria Benoit <vrose834@gmail.com>
2023-06-21 12:29:13 -0300

    Added responsiveness to the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,37 +5,41 @@ import img2 from '../images/interior/img2.jpg';
 import img7 from '../images/food/img7.jpg';
 import img5 from '../images/food/img5.jpg';
 import img6 from '../images/food/img6.jpg';
-import { HomeDiv } from '../styled/Home.styled';
+import { BorderDiv, HomeDiv, HomeDivName } from '../styled/Home.styled';
 
 const HomePage = () => {
     return (
     <div>
     <Carousel autoplay>
     <div className='carouselDiv'>
-    <img src={img1} alt="restaurant" height="400px" width="100%" overflow="hidden"></img>
+    <img src={img1} alt="restaurant" height="400px" className='responsiveImg'></img>
     </div>
     <div>
-    <img src={img2} alt="restaurant" height="400px" width="100%" overflow="hidden"></img>
+    <img src={img2} alt="restaurant" className='responsiveImg'></img>
     </div>
     <div>
-    <img src={img7} alt="spaghetti" height="400px" width="100%" overflow='hidden' object-fit="cover"></img>
+    <img src={img7} alt="spaghetti" className='responsiveImg'></img>
     </div>
   </Carousel>
 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
   <Col span={12}  className='homeDiv'>
-    <img src={img5} alt='food' height='400px' width="100%"></img>
+    <img src={img5} alt='food' className='responsiveImg'></img>
     </Col>
   <Col span={12}  className='homeDiv'>
     <HomeDiv>
-      <h3>Menu</h3>
+      <BorderDiv>
+      <HomeDivName>Menu</HomeDivName>
+      </BorderDiv>
       </HomeDiv></Col>
       <Col span={12}  className='homeDiv'> 
       <HomeDiv>
-      <h3>Contact Us</h3>
+        <BorderDiv>
+      <HomeDivName>Contact Us</HomeDivName>
+      </BorderDiv>
       </HomeDiv>
       </Col>
   <Col span={12}  className='homeDiv'>
-    <img src={img6} alt='food' height='400px' width='100%'></img>
+    <img src={img6} alt='food' className='responsiveImg'></img>
     </Col>
 </Row>
 
--- client/src/styled/Home.styled.js
+++ client/src/styled/Home.styled.js
@@ -5,4 +5,23 @@ background-color: ${({theme}) => theme.colors.offWhite};
 box-shadow: 10px lightgrey;
 width: 100%;
 height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
+
+`;
+export const BorderDiv = styled.div `
+border: 2px solid ${({theme}) => theme.colors.navy};
+border-radius: 5px;
+height: 80%;
+width: 80%;
+padding: 3em;
+`;
+export const HomeDivName = styled.h2 `
+padding-top: 3em;
+&:hover{
+    color: ${({theme}) => theme.colors.orange};
+}
 `;
