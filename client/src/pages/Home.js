a0e9b5956a3d705691f0736031fdd13d016ce4b0
Victoria Benoit <vrose834@gmail.com>
2023-06-21 11:58:53 -0300

    Home page 4 pictures on the bottom

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,51 +1,44 @@
-import { Col, Divider, Row } from 'antd';
+import { Col, Row } from 'antd';
 import { Carousel } from 'antd';
 import img1 from '../images/interior/img1.jpg';
 import img2 from '../images/interior/img2.jpg';
-
+import img7 from '../images/food/img7.jpg';
+import img5 from '../images/food/img5.jpg';
+import img6 from '../images/food/img6.jpg';
+import { HomeDiv } from '../styled/Home.styled';
 
 const HomePage = () => {
-    return ( <div>
-        
-    <h1>Home Page</h1>
-
-
-
-
-
-  <Carousel autoplay>
+    return (
     <div>
-    <img src={img1} alt="restaurant" height="500" width="100%"></img>
+    <Carousel autoplay>
+    <div className='carouselDiv'>
+    <img src={img1} alt="restaurant" height="400px" width="100%" overflow="hidden"></img>
     </div>
     <div>
-    <img src={img2} alt="restaurant" height="500" width="100%"></img>
+    <img src={img2} alt="restaurant" height="400px" width="100%" overflow="hidden"></img>
     </div>
     <div>
-    <img src={img1} alt="restaurant" height="500" width="100%"></img>
-    </div>
-    <div>
-    <img src={img2} alt="restaurant" height="500" width="100%"></img>
+    <img src={img7} alt="spaghetti" height="400px" width="100%" overflow='hidden' object-fit="cover"></img>
     </div>
   </Carousel>
-
-
-
-
-
-  <Row>
-    <Col xs={{ span: 2, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-      Col
+<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+  <Col span={12}  className='homeDiv'>
+    <img src={img5} alt='food' height='400px' width="100%"></img>
     </Col>
-    <Col xs={{ span: 20, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-      Col
+  <Col span={12}  className='homeDiv'>
+    <HomeDiv>
+      <h3>Menu</h3>
+      </HomeDiv></Col>
+      <Col span={12}  className='homeDiv'> 
+      <HomeDiv>
+      <h3>Contact Us</h3>
+      </HomeDiv>
+      </Col>
+  <Col span={12}  className='homeDiv'>
+    <img src={img6} alt='food' height='400px' width='100%'></img>
     </Col>
-    <Col xs={{ span: 2, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-      Col
-    </Col>
-  </Row>
+</Row>
 
-    
-    
     </div> );
 }
  
---
+++ client/src/styled/Home.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HomeDiv = styled.div `
+background-color: ${({theme}) => theme.colors.offWhite};
+box-shadow: 10px lightgrey;
+width: 100%;
+height: 100%;
+`;
