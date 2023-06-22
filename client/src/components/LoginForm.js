c19b28d9e8b354eabdfcc968bcf926c29d77f6ab
Victoria Benoit <vrose834@gmail.com>
2023-06-22 10:59:52 -0300

    Fixed the login and signup pages may work now

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,51 +1,53 @@
 import { useState } from "react";
-//Import the LOGIN_USER Mutation once created
+import { LOGIN_USER} from '../utils/mutations';
 import {useMutation} from '@apollo/client';
-//import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 import { StyledLogin } from "../styled/Login.styled";
 
-const LoginForm = () => {
-    const [formData, setFormData] = useState({email: '', password: ''});
-   // const [login, {error}] = useMutation(SIGNUP_USER);
+const LoginForm = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN_USER);
 
-    const handleInputChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({...formData, [name]: value})
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
     };
 
-    const handleFormSubmit = async(e) => {
-        e.preventDefault();
-        console.log(formData);
-        try{
-           // const {data} = await login({
-          //      variables: {...formData}
-           // });
-           // console.log(data);
-          //  Auth.login(data.login.token);
-        }catch (err){
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token);
+        } catch (err) {
             console.error(err);
-            //Add an alert right here
         }
-        setFormData({
+
+        setFormState({
             email: '',
-            password: ''
-        })
-    }
+            password: '',
+        });
+    };
 
     return (
 
 <div>
-
       <StyledLogin onSubmit={handleFormSubmit}>
         <h3>Login</h3>
             <label>Email: </label>
-            <input type="email" value={formData.email} name='email' required/>
+            <input type="email" value={formState.email} name='email' onChange={handleChange} required/>
             <label>Password:</label>
-            <input type="password" value={formData.password} name='password'required/>
+            <input type="password" value={formState.password} name='password' onChange={handleChange} required/>
             <button className="button">Submit</button>
+            <h2>{error ? error : ''}</h2>
         </StyledLogin>
-
-
     </div>
 );
     }
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,28 +1,53 @@
 import { useState } from "react";
-
-//Import the SIGNUP_USER Mutation once created
 import {useMutation} from '@apollo/client';
-//import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 import { StyledLogin } from "../styled/Login.styled";
 
 const SignupForm = () => {
-    const[formData, setFormData] = useState({username: '', email: '', password: ''});
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+    const [addUser, {error}] = useMutation(ADD_USER);
 
-    const submitForm = () => {
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    }
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState},
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return ( 
     <div>
         
-        <StyledLogin onSubmit={submitForm}>
+        <StyledLogin onSubmit={handleFormSubmit}>
         <h3>Sign Up</h3>
             <label>Username: </label>
-            <input type="text" value={formData.username} name='name' required />
+            <input type="text" value={formState.username} name='name'onChange={handleChange} required />
             <label>Email: </label>
-            <input type="email" value={formData.email} name='email' required/>
+            <input type="email" value={formState.email} name='email' onChange={handleChange} required/>
             <label>Password:</label>
-            <input type="password" value={formData.password} name='password'required/>
+            <input type="password" value={formState.password} name='password' onChange={handleChange} required/>
+            <h2>{error ? error : ''}</h2>
             <button className="button">Submit</button>
         </StyledLogin>
 
