3a0ff555fc4bb6cc1a4a2215117b9a0a4e415e9a
Victoria Benoit <vrose834@gmail.com>
2023-06-22 15:41:30 -0300

    Started the customer page

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -1,11 +1,30 @@
 import { useQuery } from "@apollo/client";
+import { StyledCustomer, StyledCustomerRight, CustomerMappedReviews, CustomerReview } from "../styled/Customer.styled";
+import img4 from '../images/food/img4.jpg';
 
 const CustomerPage = () => {
-    return ( <div>
+    return ( 
+    <StyledCustomer>
+        <img src={img4} alt='table' className="leftImg"></img>
+
+       <StyledCustomerRight>
+
+        <CustomerReview>
+            <h3>Users Name</h3>
+            <form>
+                <label>Leave a review:</label>
+                <textarea name='review' rows="5" cols="60"/>
+            </form>
+
+
+        </CustomerReview>
+       <CustomerMappedReviews>
+        <h5>Customer Mapped Reviews Go Here</h5>
+       </CustomerMappedReviews>
+       </StyledCustomerRight>
+    </StyledCustomer>
         
-        Customer's Profile Page
-        
-        </div> );
+  );
 }
  
 export default CustomerPage;
