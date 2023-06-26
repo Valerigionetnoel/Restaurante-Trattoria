f38de3bf2dd65e46cb3125498ebb0f0e46b1f8a1
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-26 19:53:26 -0300

    change some things in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,16 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Review, Reservation} = require('../models');
+const { User, Review, Reservation } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async(parent, args, context) => {
-            if(context.user){
-              const user = await User.findById({_id: context.user._id}).populate('reviews');
-              return user;
-            } 
-                throw new AuthenticationError('User is not logged in')
-        
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById({ _id: context.user._id }).populate('reviews');
+                return user;
+            }
+            throw new AuthenticationError('User is not logged in')
+
         },
     },
 
@@ -38,46 +38,46 @@ const resolvers = {
         },
         addReview: async (parent, { reviewText }, context) => {
             if (context.user) {
-            console.log('Creating the review', reviewText);
-              const review = await Review.create({
-                reviewText,
-                reviewAuthor: context.user.username,
-              });
-              await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { reviews: review._id } }
-              );
-              return review;
+                console.log('Creating the review', reviewText);
+                const review = await Review.create({
+                    reviewText,
+                    reviewAuthor: context.user.username,
+                });
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { reviews: review._id } }
+                );
+                return review;
             }
             throw new AuthenticationError('You need to be logged in!');
-          },
+        },
 
-          //This needs to be completed
-          addReservation: async (parent, { reservationName, reservationDate, reservationNumber, reservationTime }, context) => {
+        //This needs to be completed
+        addReservation: async (parent, { reservationName, reservationDate, reservationNumber, reservationTime }, context) => {
             console.log('Adding a reservation', { reservationName, reservationDate, reservationNumber, reservationTime });
             if (context.user) {
-            console.log('Creating the reservation', reservationName);
-              const reservation = await Reservation.create({
-                reservationName,
-                reservationDate,
-                reservationNumber,
-                reservationTime
-              });
-              return reservation;
+                console.log('Creating the reservation', reservationName);
+                const reservation = await Reservation.create({
+                    reservationName,
+                    reservationDate,
+                    reservationNumber,
+                    reservationTime
+                });
+                return reservation;
             }
             throw new AuthenticationError('You need to be logged in!');
-          },
+        },
 
-          deleteReview: async (parent, { reviewId }) => {
+        deleteReview: async (parent, { reviewId }) => {
             console.log('Deleting a review', reviewId);
             return Review.findOneAndDelete({ _id: reviewId });
-          },
+        },
 
-          deleteReservation: async (parent,{ reservationId } ) => {
+        deleteReservation: async (parent, { reservationId }) => {
             console.log('Deleting reservation', reservationId);
-            return Reservation.findOneAndDelete({ _id: reservationId})
-          }
-    }
+            return Reservation.findOneAndDelete({ _id: reservationId })
+        },
+    },
 };
 
 module.exports = resolvers
