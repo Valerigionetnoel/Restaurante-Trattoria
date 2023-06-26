34226bb14d507ee21617c6b298145b7f59515209
Victoria Benoit <vrose834@gmail.com>
2023-06-26 13:30:50 -0300

    Started the delete a review in customer page

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -1,5 +1,5 @@
-import { useQuery} from "@apollo/client";
-import { GET_USER } from "../utils/queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_USER, DELETE_REVIEW  } from "../utils/queries";
 import { OverflowMapped, StyledCustomer, StyledCustomerRight, CustomerMappedReviews, CustomerReview } from "../styled/Customer.styled";
 import img4 from '../images/food/img4.jpg';
 import Auth from "../utils/auth";
@@ -15,6 +15,28 @@ const CustomerPage = () => {
     if(loading){
         <h2>Loading...</h2>
     }
+
+
+        //For DELETE_REVIEW
+        const [deleteReview, {error}] = useMutation(DELETE_REVIEW);
+
+        const deleteAReview = async(reviewId) => {
+          const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+
+         if (!token) {
+         return false;
+        }
+        try{
+          const {data} = await deleteReview({
+            variables: {reviewId}
+          })
+
+        } catch(error){
+          console.error(error);
+        }
+
+        }
    
     return ( 
         <>
